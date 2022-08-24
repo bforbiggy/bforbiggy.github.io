01dd1cad6e628ea4b2dd4f033c86868b18e33961
bforbiggy <AndyHeCS@protonmail.com>
2022-08-24 09:30:16 -0400

    Better formatting

--- src/pages/homepage/components/AboutMe.jsx
+++ src/pages/homepage/components/AboutMe.jsx
@@ -6,50 +6,38 @@ const AboutMe = () => {
   return (
     <section className="maron">
       <div className="content-container">
-        <div
-          className="sparkle"
-          style={{
-            top: "20%", left: "20%",
-            color: "#262853"
-          }}
-        >
-          <HiSparkles />
+
+        <div className="sparkle" style={{
+          top: "20%", left: "20%",
+          animationDuration: "3s"
+        }}>
+          <HiSparkles style={{ color: "#262853" }} />
         </div>
-        <div
-          className="sparkle"
-          style={{
-            top: "70%",
-            right: "20%",
-            color: "#051929",
-            animationDelay: "200ms",
-            animationDuration: "3.5s",
-          }}
-        >
-          <HiSparkles />
+
+        <div className="sparkle" style={{
+          top: "70%", right: "20%",
+          animationDelay: "200ms",
+          animationDuration: "3.5s",
+        }}>
+          <HiSparkles style={{ color: "#051929" }} />
         </div>
-        <div
-          className="sparkle"
-          style={{
-            top: "10%",
-            right: "15%",
-            color: "#171718",
-            animationDelay: "500ms",
-            animationDuration: "4s",
-          }}
-        >
-          <IoSparkles />
+
+        <div className="sparkle" style={{
+          top: "10%", right: "15%",
+          animationDelay: "500ms",
+          animationDuration: "4s",
+        }}>
+          <IoSparkles style={{ color: "#171718" }} />
         </div>
-        <div
-          className="sparkle"
-          style={{
-            bottom: "10%",
-            left: "20%",
-            animationDelay: "50ms",
-            animationDuration: "5s",
-          }}
-        >
-          <IoSparkles />
+
+        <div className="sparkle" style={{
+          bottom: "10%", left: "20%",
+          animationDelay: "50ms",
+          animationDuration: "5s",
+        }}>
+          <IoSparkles style={{ color: "#171718" }} />
         </div>
+
         <div className="aboutme">
           <div className="title">
             <h1>About Me</h1>
