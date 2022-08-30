300c92aded3be3c54d4fe6356ddbb831154f0996
bforbiggy <AndyHeCS@protonmail.com>
2022-08-30 09:05:56 -0400

    Fancy widdle svg, more css auuugh

--- src/pages/links/Links.jsx
+++ src/pages/links/Links.jsx
@@ -1,15 +1,25 @@
 import "../../styles/CoreStyles.scss";
 import "./Links.scss";
+import { AiFillGithub } from "react-icons/ai";
+import Flourish from "../../assets/flourish.svg";
 
 const Links = () => {
 	return (
 		<div className="links-container">
 			<div className="title">
 				<h1>Links</h1>
+				<img src={Flourish} alt="" color="#E64B2A"></img>
 			</div>
+
 			<div className="linkedlist">
+
 				<div className="link">
-					Github
+					<AiFillGithub />
+					<p>Github</p>
+				</div>
+
+				<div className="link">
+					<p>Super Big Long Link</p>
 				</div>
 			</div>
 		</div>
