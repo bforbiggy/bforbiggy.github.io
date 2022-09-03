1dec9b2e3798ad1145c533522f8309df1bb7dc34
bforbiggy <AndyHeCS@protonmail.com>
2022-09-03 08:04:32 -0400

    Liv's tiny phone..

--- src/router/SiteRouter.jsx
+++ src/router/SiteRouter.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 
 import Menu from "../core/Menu/Menu";
-
 import HomePage from "../pages/homepage/Homepage";
+
+const Bgm = React.lazy(() => import("../core/Music/BGM"));
 const Repositories = React.lazy(() => import("../pages/repositories/Repositories"));
 const Links = React.lazy(() => import("../pages/links/Links"));
-const BGM = React.lazy(() => import("../core/Music/BGM"));
 
 const SiteRouter = () => {
   return (
@@ -24,7 +24,7 @@ const SiteRouter = () => {
 const Main = () => {
   return <>
     <Menu />
-    <BGM />
+    <Bgm />
     <Outlet />
   </>
 };
