17e1fbde56e9323d2cb4259d798dd9b72f2bbf17
bforbiggy <AndyHeCS@protonmail.com>
2022-10-09 02:46:02 -0400

    more REACT-ive element creation

--- src/pages/farewell/Farewell.jsx
+++ src/pages/farewell/Farewell.jsx
@@ -1,24 +1,6 @@
 import React from "react";
 import "./Farewell.scss";
-
-let snowflakes_count = 200;
-
-function spawn_snow(snow_density = 200) {
-	snow_density -= 1;
-	for (let x = 0; x < snow_density; x++) {
-		let board = document.createElement('div');
-		board.className = "snowflake";
-		document.getElementById('snow').appendChild(board);
-	}
-}
-
-// Append style for each snowflake to the head
-function add_css(rule) {
-	let css = document.createElement('style');
-	css.type = 'text/css';
-	css.appendChild(document.createTextNode(rule));
-	document.getElementsByTagName("head")[0].appendChild(css);
-}
+import styled, { keyframes } from "styled-components";
 
 function random_int(value = 100) {
 	return Math.floor(Math.random() * value) + 1;
@@ -28,54 +10,41 @@ function random_range(min, max) {
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-function spawnSnowCSS(snow_density = 200) {
-	let snowflake_name = "snowflake";
-	let rule = ``;
-
-	for (let i = 1; i < snow_density; i++) {
+function generateFlakes(snowCount) {
+	let snow = []
+	for (let i = 0; i < snowCount; i++) {
 		let random_x = Math.random() * 100; // vw
-		let random_offset = random_range(-100000, 100000) * 0.0001; // vw;
-		let random_yoyo_time = random_range(30000, 80000) / 100000;
+		let random_offset = random_range(-100, 100) * 0.1; // vw;
+		let random_yoyo_time = random_range(3, 8) / 10;
 		let random_yoyo_y = random_yoyo_time * 100; // vh
 		let random_scale = Math.random();
 		let fall_duration = random_range(10, 30) * 1; // s
-		let fall_delay = random_int(30) * -1; // s
-		let opacity = Math.random();
 
-		rule += `
-        .${snowflake_name}:nth-child(${i}) {
-            opacity: ${opacity};
-            transform: translate(${random_x}vw, -10px) scale(${random_scale});
-            animation: fall-${i} ${fall_duration}s ${fall_delay}s linear infinite;
-        }
+		// Ignore error highlights, this compiles
+		const flakeAnimation = keyframes`
+			${random_yoyo_time * 100}% {
+      	transform: translate(${random_x + random_offset}vw, ${random_yoyo_y}vh) scale(${random_scale});
+      }
+      to {
+        transform: translate(${random_x + (random_offset / 2)}vw, 100vh) scale(${random_scale});
+      }`
 
-        @keyframes fall-${i} {
-            ${random_yoyo_time * 100}% {
-                transform: translate(${random_x + random_offset}vw, ${random_yoyo_y}vh) scale(${random_scale});
-            }
-
-            to {
-                transform: translate(${random_x + (random_offset / 2)}vw, 100vh) scale(${random_scale});
-            }
-            
-        }
-        `
-
+		let Flake = styled.div`
+			filter:
+					opacity: ${Math.random()};
+					transform: translate(${random_x}vw, -10px) scale(${random_scale});
+					animation: ${flakeAnimation} ${fall_duration}s ${-random_int(30)}s linear infinite;
+		`;
+		snow.push(<Flake className="snowflake" />);
 	}
-
-	add_css(rule);
+	return snow
 }
-
-// Load the rules and execute after the DOM loads
-window.onload = function () {
-	spawnSnowCSS(snowflakes_count);
-	spawn_snow(snowflakes_count);
-};
 
 const Farewell = () => {
 	return <>
-		<div id="snow"></div>
+		<div id="snow">
+			{generateFlakes(200)}
+		</div>
 		<div className="boring-essay">
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur scelerisque eros eu est maximus dignissim. Donec suscipit hendrerit turpis, in mollis eros tincidunt vel. Quisque imperdiet, odio eget ultricies rutrum, felis justo luctus quam, quis vulputate ligula diam eu purus. Vestibulum suscipit a libero in ornare. Nam placerat id felis id varius. Etiam ac aliquet massa. Integer luctus eu ex eu molestie. Phasellus luctus tellus sit amet enim bibendum, eget efficitur tellus luctus. Integer eu dolor sed ipsum aliquam sollicitudin egestas non quam. Ut cursus sem orci, in pellentesque odio dapibus eget.
