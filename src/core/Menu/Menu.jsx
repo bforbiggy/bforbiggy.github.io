a14e3ae14299c3fd5c1c8c2d1a770e318a0bdafe
bforbiggy <AndyHeCS@protonmail.com>
2022-10-19 20:53:25 -0400

    BGM button behind menu

--- src/core/Menu/Menu.jsx
+++ src/core/Menu/Menu.jsx
@@ -14,28 +14,30 @@ const Menu = ({ menu, setMenu }) => {
     setMenu(!menu)
   };
 
+  const menuButton = <div className={`menu-button open ${!menu || "hide"}`} onClick={ToggleMenu}>
+    <HiMenu />
+  </div >;
+
+  const menuPanel = <div className={`menu ${menu || "menu-hidden"}`}>
+    <div className="menu-container">
+      <div className={`menu-button close ${menu || "hide"}`} onClick={ToggleMenu}>
+        <HiOutlineX />
+      </div>
+
+      <div className="menu-items">
+        {pages.map(data =>
+          <NavLink to={data.link} className="item" onClick={ToggleMenu} key={data.text}>
+            {data.text}
+          </NavLink>
+        )}
+      </div>
+    </div>
+  </div>;
+
   return (
     <>
-      <div className={`menu-expander open ${!menu || "hide"}`} onClick={ToggleMenu}>
-        <HiMenu />
-      </div>
-
-      <div className={`menu ${menu || "menu-hidden"}`}>
-        <div className="menu-container">
-          <div className={`menu-expander close ${menu || "hide"}`} onClick={ToggleMenu}>
-            <HiOutlineX />
-          </div>
-
-
-          <div className="menu-items">
-            {pages.map(data =>
-              <NavLink to={data.link} className="item" onClick={ToggleMenu} key={data.text}>
-                {data.text}
-              </NavLink>
-            )}
-          </div>
-        </div>
-      </div>
+      {menuButton}
+      {menuPanel}
     </>
   );
 };
