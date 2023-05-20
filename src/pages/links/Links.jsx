2e3bff649d4b391c74ab0428efff3b56728c5cf8
bforbiggy <AndyHeCS@protonmail.com>
2023-05-20 16:34:43 -0400

    Snowing Component, Lazy Loading

--- src/pages/links/Links.jsx
+++ src/pages/links/Links.jsx
@@ -2,9 +2,8 @@ import "../../styles/CoreStyles.scss";
 import "./Links.scss";
 import { AiFillGithub, AiOutlineMail, AiOutlineTwitter, AiFillLinkedin } from "react-icons/ai";
 import { SiLeetcode, SiDiscord } from "react-icons/si";
-import { isMobile, randomInteger } from "../../core/Util";
-import React, { useEffect, useState } from "react";
-import styled, { keyframes } from "styled-components";
+import React, { lazy, Suspense } from 'react';
+const Snowflakes = lazy(() => import('./Snowflakes'));
 
 const links = [
 	{ text: "Email", icon: <AiOutlineMail />, url: "mailto:andyhecsp@gmail.com" },
@@ -15,42 +14,15 @@ const links = [
 	{ text: "LeetCode", icon: <SiLeetcode />, url: "https://leetcode.com/bforbiggy/" },
 ];
 
-let flakeCount = isMobile() ? 50 : 200;
 
-let snowflakes = [];
-for (let i = 0; i < flakeCount; i++) {
-	let random_x = randomInteger(0, 100);
-	let offset = randomInteger(-10, 10);
-	let yoyo_time = randomInteger(30, 80);
-	let random_scale = Math.random();
-
-	const flakeAnimation = keyframes`
-			${yoyo_time}% { transform: translate(${random_x + offset}vw, ${yoyo_time}vh) scale(${random_scale});}
-			to { transform: translate(${random_x + offset / 2}vw, 100vh) scale(${random_scale});}
-		`;
-
-	let Flake = styled.div`
-			filter:
-				opacity: ${Math.max(0.1, Math.random())};
-				transform: translate(${random_x}vw, -10px);
-				animation: ${flakeAnimation} ${randomInteger(10, 30)}s ${-randomInteger(30)}s linear infinite;
-		`;
-	snowflakes.push(<Flake className="snowflake" key={`flake${i}`} />);
-}
 
 const Links = () => {
-	const [flakes, setFlakes] = useState([]);
-
-	useEffect(() => {
-		setFlakes(snowflakes);
-	}, []);
-
 	return <>
-		<div className="snow" id="snow">{flakes}</div>
-
+		<Suspense >
+			<Snowflakes />
+		</Suspense>
 
 		<div className="links-container">
-
 			<h1>Links</h1>
 
 			<div className="linkedlist">
