50727d102449e303017cd64ff94c49c9673934dc
bforbiggy <AndyHeCS@protonmail.com>
2023-09-28 02:37:58 -0400

    Updates

---
+++ static/js/434.1f07012b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbforbiggy=self.webpackChunkbforbiggy||[]).push([[434],{405:function(e,r,i){i.r(r),i.d(r,{default:function(){return n}});var l=i(184),n=function(){return(0,l.jsx)("div",{id:"movie",className:"movie",children:(0,l.jsx)("iframe",{title:"minions",src:"https://archive.org/embed/minions-the-rise-of-gru-2022",frameborder:"0",webkitallowfullscreen:"true",mozallowfullscreen:"true",allowfullscreen:!0})})}}}]);
+//# sourceMappingURL=434.1f07012b.chunk.js.map
