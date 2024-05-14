aaf85438ff131ce6f6f7db506fed69ee080657a1
bforbiggy <andyhecsp@gmail.com>
2024-05-14 02:17:46 -0400

    MUSIIIIIIIC

--- src/lib/core/Music.svelte
+++ src/lib/core/Music.svelte
@@ -1,5 +1,13 @@
 <script>
-	import bgm from "$lib/assets/bgm.mp3";
+	import BGMAudio from "$lib/assets/bgm.mp3";
+	import { getContext } from "svelte";
+
+	let paused = true;
+
+	const bgm = getContext("bgm");
+	bgm.subscribe((value) => {
+		paused = !value;
+	});
 </script>
 
-<audio src={bgm}></audio>
+<audio autoplay bind:paused src={BGMAudio}></audio>
