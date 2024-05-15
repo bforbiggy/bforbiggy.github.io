158e978eb610686fc2bd6ee0f44836cb2c171b82
bforbiggy <andyhecsp@gmail.com>
2024-05-15 15:58:23 -0400

    MAIN PAGE GRAAAAH

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,17 @@
 <script>
-	import WavesTop from "$lib/assets/waves-top.svg";
-	import WavesBottom from "$lib/assets/waves-bottom.svg";
+	import Waves from "$lib/assets/waves.svg";
 
 	let slammed = false;
 </script>
 
 <div
-	class="min-w-screen min-h-screen flex justify-center items-center
+	class="min-w-screen min-h-screen flex flex-col justify-between items-center
 	{slammed && 'animate-[rumble_0.4s]'}"
 >
-	<!-- Waves -->
+	<!-- Top Waves -->
 	<img
-		class="-z-40 absolute top-0 left-0 min-w-[2000px] w-screen bg-contain md:bg-cover"
-		src={WavesTop}
-		alt=""
-	/>
-	<img
-		class="-z-40 absolute bottom-0 left-0 min-w-[2000px] w-screen bg-contain md:bg-cover"
-		src={WavesBottom}
+		class="-z-40 min-w-[2000px] w-screen bg-contain md:bg-cover"
+		src={Waves}
 		alt=""
 	/>
 
@@ -33,13 +27,15 @@
 	</div>
 
 	<!-- Centerpiece -->
-	<div class="absolute flex-col transition-all">
+	<div
+		class="flex-col transition-all rounded-full drop-shadow-md bg-slate-800 border-4 border-red-600"
+	>
 		<button
 			class="z-50 shadow-[0px_1rem_2rem_theme('colors.red.600')] rounded-full bg-red-600"
 			on:click={() => (slammed = true)}
 		>
 			<img
-				class="w-[var(--max-radius)] h-[var(--max-radius)] rounded-full drop-shadow-md border-4 border-red-600
+				class="w-[var(--max-radius)] h-[var(--max-radius)] rounded-full
 			{!slammed && 'hover:-translate-y-14 transition-all'}
 			{slammed && 'transition-all duration-75'}"
 				src="https://i.imgur.com/fV9s2su.jpg"
@@ -47,25 +43,28 @@
 			/>
 		</button>
 
-		{#if slammed}
-			<div
-				class="-z-10 flex-col w-[var(--max-radius)] -translate-y-[var(--max-radius)] text-center break-words rounded-full py-12 bg-slate-600 border-4 border-white text-white"
-			>
-				<div class="w-full h-[500px] max-h-[90vw]" />
-				<p class="text-4xl">About Me</p>
-				<p class="px-[75px]">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et
-					turpis eu nisl imperdiet imperdiet ut sed nulla. Etiam ante enim,
-					pretium vel cursus et, accumsan vitae mi. Suspendisse potenti. Proin
-					vestibulum eros et nulla convallis consectetur. Quisque eget suscipit
-					turpis. Maecenas ut ipsum nec neque vestibulum consequat non nec orci.
-					Etiam sed elit eu felis vehicula sagittis. Cras porttitor diam mauris,
-					sit amet pellentesque tellus suscipit eget. Aliquam id magna
-					fermentum, ornare nisl in, consequat purus.
-				</p>
-			</div>
-		{/if}
+		<div
+			class="-z-50 w-[var(--max-radius)] flex flex-col gap-4 text-center break-words overflow-hidden rounded-full
+			px-12 {slammed ? 'h-auto pt-6 pb-20' : 'h-0 p-0'} transition-all"
+		>
+			<p class="text-4xl text-white">[ABOUTME.md]</p>
+			<p class="text-white">
+				I'm a 21 year old developer looking to build the next big thing! My full
+				stack experience and passion for learning makes me a well-rounded and
+				valuable asset to any team, large or small.
+			</p>
+			<p class="text-sm text-white opacity-50">
+				Hobbies Include: Art, Piano, Boxing
+			</p>
+		</div>
 	</div>
+
+	<!-- Bottom Waves -->
+	<img
+		class="-z-40 min-w-[2000px] w-screen rotate-180 bg-contain md:bg-cover"
+		src={Waves}
+		alt=""
+	/>
 </div>
 
 <style>
