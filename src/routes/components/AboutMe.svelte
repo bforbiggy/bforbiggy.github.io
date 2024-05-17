de17d64e257f9ed39214d75b5786ef884e79dfba
bforbiggy <andyhecsp@gmail.com>
2024-05-16 23:59:41 -0400

    About Me Section

--- src/routes/components/AboutMe.svelte
+++ src/routes/components/AboutMe.svelte
@@ -1,13 +1,20 @@
 <div
-	class="w-screen bg-[theme('colors.haikei-red')] p-[5em] flex flex-col justify-center items-center"
+	class="w-screen bg-[theme('colors.haikei-red')] p-[5em] flex flex-col justify-center items-center gap-12 font-mono text-white text-center"
 >
-	<h1 class="text-7xl text-white font-bold font-mono">About Me</h1>
-	<p class="max-w-[1000px] text-2xl text-bold">
-		Hello! On the internet I appear under the name <span
-			class="text-emerald-500">biggy</span
-		>, as a 20 years old Fullstack developer && software engineering student.
-		I'm a 21 year old developer looking to build the next big thing! My full
-		stack experience and passion for learning makes me a well-rounded and
-		valuable asset to any team, large or small.
+	<h1 class="text-7xl text-white font-bold">About Me</h1>
+	<p class="max-w-[800px] text-2xl text-bold">
+		Hi! On the internet I appear under the name
+		<span class="text-emerald-300">biggy</span>, as a
+		<span class="text-emerald-300">21</span>
+		years old Fullstack developer <span class="text-emerald-300">&&</span> software
+		engineer looking to build the next big thing!
+	</p>
+	<p class="max-w-[800px] text-2xl text-bold">
+		My full stack experience and passion for learning makes me a well-rounded
+		and valuable asset to any team, large or small. Whether it be centering a <span
+			class="text-emerald-300">div</span
+		>
+		or designing a <span class="text-emerald-300">backend</span>, I am ready to
+		tackle any challenge.
 	</p>
 </div>
