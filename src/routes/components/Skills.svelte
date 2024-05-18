7652a9d74f5bc6e4844bec47fb3395ff8e175854
bforbiggy <andyhecsp@gmail.com>
2024-05-18 01:19:00 -0400

    Skills

--- src/routes/components/Skills.svelte
+++ src/routes/components/Skills.svelte
@@ -70,16 +70,36 @@
 >
 	<WavesTopDark classStyles="z-20 min-w-[2000px] w-screen" />
 	<div class="flex flex-col items-center">
-		<h1 class="text-5xl text-white text-middle">Skills</h1>
-		<span class="w-14 h-14 icon-[ri--fire-fill]" style="color: white;" />
-		<div class="flex flex-row flex-wrap justify-center max-w-[1000px] p-2">
-			{#each SKILLS as skill}
-				<div class="group flex flex-col m-4">
-					<i href={skill.link} class="text-6xl rounded {skill.icon}"></i>
-					<div class="invisible group-hover:visible text-white text-center">
+		<div class="flex flex-col items-center gap-3 peer">
+			<span
+				class="w-14 h-14 icon-[ri--fire-fill]"
+				style="color: rgb(244 63 94);"
+			/>
+			<h1 class="text-5xl text-white text-middle">Skills</h1>
+			<div class="w-[800px] max-w-[100vw] h-[1px] border border-rose-500"></div>
+		</div>
+
+		<div class="flex flex-row flex-wrap justify-center max-w-[1000px] py-5">
+			{#each SKILLS as skill, i}
+				{@const isEven = i % 2 === 0}
+				<div class="group flex flex-col items-center m-4 bg-inherit">
+					<i
+						href={skill.link}
+						class="text-6xl {skill.icon} rounded p-5 mx-5 mt-5 border border-opacity-0 group-hover:border-opacity-100
+						{isEven ? 'border-emerald-500' : 'border-rose-500'} duration-150"
+					></i>
+					<div
+						class="w-[1px] h-4 border
+						{isEven ? 'border-emerald-500' : 'border-rose-500'} 
+							opacity-0 group-hover:opacity-100 duration-150"
+					/>
+					<div
+						class="text-xl text-white border-2 rounded-md
+						{isEven ? 'border-emerald-500' : 'border-rose-500'} 
+						px-3 py-2 opacity-0 group-hover:opacity-100 duration-150"
+					>
 						{skill.name}
 					</div>
-					<div class="" />
 				</div>
 			{/each}
 		</div>
