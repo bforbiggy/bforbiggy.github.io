f63ea5867f9bd21d5f42882856e5599cb29918e3
bforbiggy <andyhecsp@gmail.com>
2024-05-19 09:30:49 -0400

    StackedSteps Links

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,3 +1,8 @@
-<div class="w-screen h-screen bg-slate-900 flex justify-center items-center">
+<script>
+</script>
+
+<div
+	class="w-screen h-screen bg-slate-900 flex justify-center items-center bg-[url('$lib/assets/DarkStackedSteps.svg')]"
+>
 	<p class="text-white">Empty right now!</p>
 </div>
