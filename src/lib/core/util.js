32099de8bb6f522bb1cb67fdc44b1216583773ab
bforbiggy <andyhecsp@gmail.com>
2024-05-21 00:35:09 -0400

    Tarot card links!!!!

--- src/lib/core/util.js
+++ src/lib/core/util.js
@@ -8,4 +8,20 @@ function randomRange(min, max) {
 	return Math.random() * (max - min) + min;
 }
 
-export { randomRange }
+function shuffle(array) {
+	let currentIndex = array.length;
+
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+
+		// Pick a remaining element...
+		let randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex], array[currentIndex]];
+	}
+}
+
+export { randomRange, shuffle }
--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,10 +1,50 @@
 <script>
 	import { onMount } from "svelte";
+	import { shuffle } from "lodash-es";
+	import tarot from "$lib/assets/tarot.webp";
 
 	onMount(() => {});
 
-	const NUMS = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-	const SUITS = "♥️♣️♠️♦️";
+	const CARDS = shuffle(
+		Array(52)
+			.fill()
+			.map((_, i) => {
+				const NUMS = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+				const SUITS = ["♦️", "♣️", "♥️", "♠️"];
+				return {
+					suit: SUITS[i % 4],
+					num: NUMS[i % 13],
+				};
+			})
+	);
+
+	const LINKS = [
+		{
+			name: "Email",
+			url: "mailto:andyhecsp@gmail.com",
+			icon: "icon-[tabler--mail-filled]",
+		},
+		{
+			name: "GitHub",
+			url: "https://github.com/bforbiggy",
+			icon: "icon-[tabler--brand-github]",
+		},
+		{
+			name: "LinkedIn",
+			url: "https://www.linkedin.com/in/andy-hecsp/",
+			icon: "icon-[tabler--brand-linkedin]",
+		},
+		{
+			name: "LeetCode",
+			url: "https://leetcode.com/u/bforbiggy/",
+			icon: "icon-[tabler--brand-leetcode]",
+		},
+		{
+			name: "Discord",
+			url: "https://discord.com/users/187003715302064128",
+			icon: "icon-[tabler--brand-discord]",
+		},
+	];
 </script>
 
 <svelte:head>
@@ -17,51 +57,61 @@
 </svelte:head>
 
 <div
-	class="w-screen h-screen flex justify-center items-center overflow-x-hidden font-[roboto-slab]"
+	class="w-screen h-screen flex justify-center items-center gap-3 overflow-x-hidden font-[roboto-slab]"
 >
 	<div
 		class="absolute min-w-[2000px] w-screen h-screen bg-[url('$lib/assets/DarkStackedSteps.svg')]"
 	/>
 
-	<a
-		href="https://leetcode.com/u/bforbiggy/"
-		target="_blank"
-		class="relative w-[250px] h-[400px] group"
-	>
-		<!-- Card Face -->
-		<div
-			class="absolute inset-x-0 inset-y-0 bg-slate-950 rounded-lg
-				group-hover:blur-sm group-hover:-scale-x-100 transition-all delay-75 duration-500"
+	{#each LINKS as data, i}
+		{@const card = CARDS[i]}
+		{@const isRed = i % 2 === 0}
+		<a
+			href={data.url}
+			target="_blank"
+			class="relative w-[250px] h-[400px] group"
 		>
-			<p class="absolute top-2 left-2 text-3xl text-white">
-				{SUITS[0]}<br />{NUMS[5]}
-			</p>
-			<p class="absolute bottom-2 right-2 text-3xl text-white">
-				{SUITS[0]}<br />{NUMS[5]}
-			</p>
+			<!-- Card Face -->
 			<div
-				class="absolute inset-x-0 inset-y-0 w-full h-full
-				group-hover:blur-md group-hover:-scale-x-100 transition-all delay-75 duration-500"
+				class="absolute inset-x-0 inset-y-0 bg-slate-950 scale-x-0 rounded-lg
+				group-hover:scale-x-100 transition-all delay-300 duration-300"
 			>
-				<span
-					class="w-full h-full icon-[tabler--brand-leetcode]
-					group-hover:-scale-x-100 transition-all delay-75 duration-500"
-					style="color: white;"
-				/>
+				<p
+					class="absolute top-2 left-2 text-3xl
+					{isRed ? 'text-red-500' : 'text-white'}"
+				>
+					{card.suit}<br />{card.num}
+				</p>
+				<p
+					class="absolute bottom-2 right-2 text-3xl
+					{isRed ? 'text-red-500' : 'text-white'}"
+				>
+					{card.suit}<br />{card.num}
+				</p>
+				<div class="absolute inset-x-0 inset-y-0 w-full h-full">
+					<span
+						class="w-full h-full {data.icon} 
+						{isRed ? 'text-red-500' : 'text-white'}"
+					/>
+				</div>
 			</div>
-		</div>
 
-		<!-- Card Back -->
-		<div
-			class="absolute inset-x-0 inset-y-0 m-auto flex justify-center items-center"
-		>
-			<p
-				class="
-				text-5xl text-center text-white text-bold select-none
-				scale-x-0 group-hover:scale-x-100 group-hover:delay-[325ms] transition-all"
+			<!-- Card Back -->
+			<div
+				class="absolute inset-x-0 inset-y-0 m-auto flex justify-center items-center
+				group-hover:scale-x-0 transition-all delay-300 group-hover:delay-0 duration-300"
 			>
-				Leetcode
-			</p>
-		</div>
-	</a>
+				<img
+					class="absolute inset-x-0 inset-y-0 w-full h-full rounded-lg"
+					src={tarot}
+					alt=""
+				/>
+				<p
+					class="z-10 text-3xl text-center text-amber-200 text-bold select-none"
+				>
+					{data.name}
+				</p>
+			</div>
+		</a>
+	{/each}
 </div>
