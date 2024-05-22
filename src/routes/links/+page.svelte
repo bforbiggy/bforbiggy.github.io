837abfce80adff8c8b64f5d171d05aad9c467e29
bforbiggy <andyhecsp@gmail.com>
2024-05-21 23:42:48 -0400

    Card animation isn't super spammable

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -18,7 +18,7 @@
 			})
 	);
 
-	const LINKS = [
+	let LINKS = [
 		{
 			name: "Email",
 			url: "mailto:andyhecsp@gmail.com",
@@ -44,7 +44,12 @@
 			url: "https://discord.com/users/187003715302064128",
 			icon: "icon-[tabler--brand-discord]",
 		},
-	];
+	].map((data) => ({
+		...data,
+		active: false,
+		face: "scale-x-0",
+		back: "scale-x-100",
+	}));
 </script>
 
 <svelte:head>
@@ -69,32 +74,45 @@
 		{#each LINKS as data, i}
 			{@const card = CARDS[i]}
 			{@const isRed = i % 2 === 0}
+			<!-- svelte-ignore a11y-mouse-events-have-key-events -->
 			<a
 				href={data.url}
 				target="_blank"
-				class="relative min-w-[250px] max-w-[250px] min-h-[400px] max-h-[400px] group"
+				class="relative min-w-[250px] max-w-[250px] min-h-[400px] max-h-[400px]"
+				on:mouseover={async () => {
+					data.face = "scale-x-100";
+					data.back = "scale-x-0";
+					data.active = true;
+					data.finished = new Promise((resolve) => setTimeout(resolve, 600));
+				}}
+				on:mouseleave={async () => {
+					await data.finished;
+					data.face = "scale-x-0";
+					data.back = "scale-x-100";
+					data.active = false;
+				}}
 			>
 				<!-- Card Face -->
 				<div
-					class="absolute inset-x-0 inset-y-0 bg-slate-950 scale-x-0 rounded-lg
-						group-hover:scale-x-100 transition-all delay-300 duration-300"
+					class="absolute inset-x-0 inset-y-0 bg-slate-950 rounded-lg
+						{data.face} transition-all delay-300 duration-300"
 				>
 					<p
 						class="absolute top-2 left-2 text-3xl
-					{isRed ? 'text-red-500' : 'text-white'}"
+							{isRed ? 'text-red-500' : 'text-white'}"
 					>
 						{card.suit}<br />{card.num}
 					</p>
 					<p
 						class="absolute bottom-2 right-2 text-3xl
-					{isRed ? 'text-red-500' : 'text-white'}"
+							{isRed ? 'text-red-500' : 'text-white'}"
 					>
 						{card.suit}<br />{card.num}
 					</p>
 					<div class="absolute inset-x-0 inset-y-0 w-full h-full">
 						<span
 							class="w-full h-full {data.icon} 
-						{isRed ? 'text-red-500' : 'text-white'}"
+								{isRed ? 'text-red-500' : 'text-white'}"
 						/>
 					</div>
 				</div>
@@ -102,7 +120,7 @@
 				<!-- Card Back -->
 				<div
 					class="absolute inset-x-0 inset-y-0 m-auto flex justify-center items-center
-						group-hover:scale-x-0 transition-all delay-300 group-hover:delay-0 duration-300"
+						{data.back} {!data.active && 'delay-[600ms]'} transition-all duration-300"
 				>
 					<img
 						class="absolute inset-x-0 inset-y-0 w-full h-full rounded-lg"
