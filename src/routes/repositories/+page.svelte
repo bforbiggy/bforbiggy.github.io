97c3bbf05700d98173578d7bc67cf877bc2c31f4
bforbiggy <andyhecsp@gmail.com>
2024-05-24 03:55:00 -0400

    Basic repo page

--- src/routes/repositories/+page.svelte
+++ src/routes/repositories/+page.svelte
@@ -1,9 +1,43 @@
+<script>
+	import { onMount } from "svelte";
+
+	let repos = null;
+	let user = null;
+
+	onMount(async () => {
+		console.log("WTF");
+		let response, data;
+
+		response = await fetch("https://api.github.com/users/bforbiggy/repos");
+		data = await response.json();
+		repos = data
+			.filter((item) => !item.fork)
+			.sort(
+				(a, b) => b.stargazers_count - a.stargazers_count || b.size - a.size
+			);
+
+		response = await fetch("https://api.github.com/users/bforbiggy");
+		data = await response.json();
+		user = data;
+	});
+</script>
+
 <div
 	class="h-screen min-w-[100vw] max-w-[100vw] bg-slate-950
 			flex justify-center p-28"
 >
-	<div
-		class="w-16 h-16 bg-white
-			flex flex-col"
-	></div>
+	{#if !user}
+		<p>Waiting....</p>
+	{:else}
+		<div
+			class="w-min h-min border-2 border-red-500
+			flex flex-col text-red-500"
+		>
+			<img class="max-w-[300px] rounded-full" src={user.avatar_url} alt="" />
+			<p>{user.login}</p>
+			<p>{user.bio}</p>
+			<p>$ Repositories ~{user.public_repos}</p>
+			<p>$ Followers ~{user.followers}</p>
+		</div>
+	{/if}
 </div>
